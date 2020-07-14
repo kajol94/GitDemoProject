501208901eb3335b708f9f71b5a0cb5b85d19b3b
this pc <this pc@user>
2020-07-14 20:54:17 +0530

    First commit

---
+++ src/GitPackage/Test.java
@@ -0,0 +1,16 @@
+package GitPackage;
+
+public class Test {
+	public static void main(String args[])
+	{
+		String hungry = "Yes";
+		if(hungry == "Yes")
+		{
+			System.out.println("Eat Something.........");
+		}
+		else
+		{
+			System.out.println("Not Eat Something.........");
+		}
+	}
+}
